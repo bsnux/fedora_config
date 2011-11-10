72a305d25cabd20bbf663dd6dc8f98e54791e733
Arturo Fernandez <arturo@bsnux.com>
2011-11-10 20:35:23 +0100

    How to add new fonts on Fedora

--- improving_fonts_fedora.sh
+++ improving_fonts_fedora.sh
@@ -3,8 +3,17 @@
 # Enabling auto-hint and anti-aliasing
 ln -s /etc/fonts/conf.avail/10-autohint.conf /etc/fonts/conf.d/
 ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+
+# Installing font renderer
+yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
 yum install freetype-freeworld
 
 #----- Reboot now before continuing
 
 fc-cache
+
+# Installing new fonts
+mkdir /usr/share/fonts/myfonts
+chmod 755 -R /usr/share/fonts/myfonts
+cd /usr/share/fonts
+fc-cache myfonts
